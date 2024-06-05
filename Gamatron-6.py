400459263a757ae12880ef3ac13634e565a88f82
zev <z0504167424@gmail.com>
2024-06-05 11:48:38 +0300

    git remote add origin https://github.com/zevpaneth/zev.git git branch -M main git push -u origin main

--- Gamatron-6.py
+++ Gamatron-6.py
@@ -1,18 +1,26 @@
 from Buble_sort import bubble_sort
 
+
+
 file_name = input(f'please write the full name of the file: ')
 
 with open(file_name, "r") as file:
-
-
-    # להציג כמה משפטים יש בטקסט כאשר משפטים מופרדים בנקודה
-
-    counter = 0
     content = file.read()
-    for char in content:
+# להציג כמה משפטים יש בטקסט כאשר משפטים מופרדים בנקודה
+
+
+def count_sentences(content):
+    counter = 0
+
+    index_counter = 0
+    for i, char in enumerate(content):
         if char == ".":
-            counter += 1
+            if i + 1 < len(content) and (content[i + 1] == " " or content[i + 1] == "\n"):
+                if i + 2 < len(content) and content[i + 2].isupper():
+                    counter += 1
     print(f'This file contains {counter} sentences')
+
+def common_word(content):
 
     # להציג את המילה הכי נפוצה בטקסט וכמה פעמים היא מופיעה
 
@@ -20,7 +28,7 @@ with open(file_name, "r") as file:
     array = []
     word = ""
     for char in content:
-        if char == " ":
+        if char == " " or char == '\n':
             array.append(word)
             word = ""
         else:
@@ -37,18 +45,33 @@ with open(file_name, "r") as file:
         if array_sorted[word] == array_sorted[word + 1]:
             most_common_word = array_sorted[word]
             counter += 1
-        else:
-            if main_counter_most_common_word["shows"] < counter:
+        elif main_counter_most_common_word["shows"] < counter:
                 main_counter_most_common_word["most common word"] = most_common_word
                 main_counter_most_common_word["shows"] = counter
 
     print(main_counter_most_common_word)
 
+
+def longest_word(content):
     # להציג את המילה הכי ארוכה בטקסט, אם יש כמה מילים באותו האורך, להציג
     # את כולן
+    # רשימה של כל המילים
+    array = []
+    word = ""
+    for char in content:
+        if char == " " or char == '\n':
+            array.append(word)
+            word = ""
+        else:
+            word += char
+
+    # ממיין את המילים
+    array_sorted = bubble_sort(array)
+
     counter = 0
     longest_word = None
     main_counter_longest_word = {"longest word": longest_word, "counter": counter}
+
     for word in array:
         if len(word) > counter:
             counter = len(word)
@@ -61,9 +84,22 @@ with open(file_name, "r") as file:
 
     print(main_counter_longest_word)
 
-
+def different_words(content):
     # להציג את כמות המילים השונות שיש בטקסט
+    array = []
+    word = ""
+    for char in content:
+        if char == " " or char == '\n':
+            array.append(word)
+            word = ""
+        else:
+            word += char
+
+    # ממיין את המילים
+    array_sorted = bubble_sort(array)
+
     amount_different_words = 0
+
     different_words = array_sorted
     i = 0
     while i < len(different_words) - 1:
@@ -76,25 +112,25 @@ with open(file_name, "r") as file:
 
 
     #להציג את כל המילים התואמות בכמות הופעתן בטקסט ע"פ קלט משתמש
-    amount = int(input('Please enter the number of impressions of the word: '))
-    # לאתחל המשתנים
+    # amount = int(input('Please enter the number of impressions of the word: '))
+    # # לאתחל המשתנים
     array = []
     word = ""
     for char in content:
-        if char == " ":
-            array.append(word)
-            word = ""
-        else:
-            word += char
+         if char == " ":
+             array.append(word)
+             word = ""
+         else:
+             word += char
 
     array_sorted = bubble_sort(array)
 
     # להציג את כל המילים התואמות בכמות הופעתן בטקסט ע"פ קלט משתמש
-    # רציתי כאן לעשות אינדקס וכהה לעדכן בFOR את המילים שהופעתם הוא במספר הנקוב
-    # for word in array_sorted:
+    # רציתי כאן לעשות אינדקס וככה לעדכן בFOR את המילים שהופעתם הוא במספר הנקוב
+    for word in array_sorted:
         #לא הספקתי לסיים
 
 
-
-
-
+count_sentences(content)
+common_word(content)
+different_words(content)
